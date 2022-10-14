2c38839bcf8d50e52d3962719d33cb2ad0396255
Carl Javier <carl.javier@hashicorp.com>
2022-10-14 15:07:04 +1100

    Add API Workflow

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    hostname     = "tfcarl.is.hashicorpdemo.com"
-    organization = "carlcorp"
-    workspaces {
-      name = "tfe-tfc-cli-migrate"
-    }
-  }
-}
